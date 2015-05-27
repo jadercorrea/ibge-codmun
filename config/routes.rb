271ca1a30e51c453cbed3c6fb0ac226864f526be
Jader Correa <jadercorrea.hv@gmail.com>
2015-05-26 22:37:09 -0300

    Add route to /codes/city and ruby version

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'codes/', to: 'codes#index'
   get '/code/:nome_municipio', to: 'codes#show'
-
-  root 'codes#index'
+  get '/codes/:nome_municipio', to: 'codes#index'
 end
